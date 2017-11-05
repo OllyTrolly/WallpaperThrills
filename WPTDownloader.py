17f393a8f855de364ebb9c4c17d3d4e114accb75
Oliver Stevenson <ollystevenson+dev@gmail.com>
2017-11-05 00:20:35 +0000

    Changed config file to be a python module instead of an INI. Removed OS- specific library win32com. Added main. Made script PEP-8 compliant.

--- WPTDownloader.py
+++ WPTDownloader.py
@@ -1,78 +1,86 @@
-#Boilerplate for pynsist
-#!python3.4
-import sys
-sys.path.insert(1, 'pkgs')
+#!python3.6
 
-from bs4 import BeautifulSoup
-from win32com.shell import shell, shellcon
+# IMPORTS #
+
+# Own packages
+import config
+
+# Stdlib packages
 import re
 import os
 import errno
 import urllib.request
-import configparser
+import sys
 
-#Read in variables from config.ini
-config = configparser.RawConfigParser()
-configPath = os.path.dirname(os.path.realpath(__file__)) + "\config.ini"
-config.read(configPath)
+# Library packages
+from bs4 import BeautifulSoup
 
-myWidth = config.getint('Resolution', 'width')
-myHeight = config.getint('Resolution', 'height')
-fuzziness = config.getint('Resolution', 'fuzziness')
+sys.path.insert(1, 'pkgs')
 
-images = config.getint('Download', 'images')
-if images > 20:
-	images = 20
-shuffle = config.getboolean('Download', 'shuffle')
 
-measure = config.get('Update', 'measure')
-number = config.getint('Update', 'number')
+def main():
 
-#Calculate desired bounds of aspect ratio
-myAspect = myWidth / myHeight
-loAspect = myAspect * (fuzziness / 100)
-hiAspect = myAspect * (2 - (fuzziness / 100))
+    # Limit of 20 to prevent hammering of server
+    if config.images > 20:
+        config.images = 20
 
-urls = []
-wallpaperDir = shell.SHGetFolderPath(0, shellcon.CSIDL_MYPICTURES, None, 0) + "/WallpaperThrills"
-	
-def getUrls():
-	#Create URL soup for parsing with Beautiful Soup
-	url = "http://deadendthrills.com"
-	if(shuffle) :
-		url = url + "/random/"
+    # Calculate desired bounds of aspect ratio
+    abs_aspect = config.width / config.height
+    low_aspect = abs_aspect * (config.fuzziness / 100)
+    high_aspect = abs_aspect * (2 - (config.fuzziness / 100))
 
-	htmlText = urllib.request.urlopen(url).read()
-	soup = BeautifulSoup(htmlText)
+    urls = []
 
-	#Find all 'a' tags with a href to a PNG
-	for a in soup.find_all('a') :
-		if a.has_attr('href') :
-			match = re.search("\.png", a.get('href'))
-			if (match) :
-				#Check aspect ratio is within desired bounds
-				width = int(a.img.get('width'))
-				height = int(a.img.get('height'))
-				detAspect = width / height
-				if (detAspect >= loAspect) and (detAspect <= hiAspect) :
-					urls.append(a.get('href'))
+    while len(urls) < config.images:
+        urls.extend(get_urls(low_aspect, high_aspect))
+    create_directory()
+    download(urls)
+    sys.exit()
 
-def createDirectory():
-	#Try to create Images directory
-	try:
-		os.makedirs(wallpaperDir)
-	except OSError as exception:
-		if exception.errno != errno.EEXIST:
-			raise
 
-def download():
-	#Download number of requested images from list of URLs
-	for x in range(1, images + 1) :
-		print("Downloading image " + str(x) + " of " + str(images))
-		urllib.request.urlretrieve(urls[x - 1], wallpaperDir + "/IMG" + str(x) + ".png")
+def get_urls(low_aspect, high_aspect):
 
-while len(urls)<images:
-	getUrls()
-createDirectory()
-download()
-sys.exit()
+    urls = []
+
+    # Create URL soup for parsing with Beautiful Soup
+    url = "http://deadendthrills.com"
+    if config.shuffle:
+        url = url + "/random/"
+
+    html_text = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html_text)
+
+    # Find all 'a' tags with a href to a PNG
+    for a in soup.find_all('a'):
+        if a.has_attr('href'):
+            match = re.search("\.png", a.get('href'))
+            if match:
+                # Check aspect ratio is within desired bounds
+                width = int(a.img.get('width'))
+                height = int(a.img.get('height'))
+                img_aspect = width / height
+                if (img_aspect >= low_aspect) and (img_aspect <= high_aspect):
+                    urls.append(a.get('href'))
+
+    return urls
+
+
+def create_directory():
+
+    # Try to create Images directory
+    try:
+        os.makedirs(config.download_location)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+
+def download(urls):
+
+    # Download number of requested images from list of URLs
+    for x in range(1, config.images + 1):
+        print("Downloading image " + str(x) + " of " + str(config.images))
+        urllib.request.urlretrieve(urls[x - 1], config.download_location + "/IMG" + str(x) + ".png")
+
+
+sys.exit(main())
---
+++ config.py
@@ -0,0 +1,26 @@
+# RESOLUTION #
+
+# Pixels wide
+width = 1920
+# Pixels high
+height = 1200
+# Accuracy of matches where 100 is exactly the specified resolution and 0 is any
+fuzziness = 80
+
+# DOWNLOAD #
+
+# Number of images to download
+images = 10
+# Whether to shuffle (default is most recent)
+shuffle = True
+
+# UPDATE #
+
+# Months, days, hours and number of them - how often you wish to download new images
+measure = "hour"
+number = 1
+
+# FILE SYSTEM #
+
+download_location = ".\\Wallpapers"
+
