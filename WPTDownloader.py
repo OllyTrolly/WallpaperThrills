ec1d201216895f867b9fa05588f19bd38bf9b69c
Oliver Stevenson <ollystevenson+dev@gmail.com>
2017-11-12 23:23:01 +0000

    Added search function for a specified game

--- WPTDownloader.py
+++ WPTDownloader.py
@@ -30,33 +30,35 @@ def main():
     low_aspect = abs_aspect * (config.fuzziness / 100)
     high_aspect = abs_aspect * (2 - (config.fuzziness / 100))
 
+    if config.shuffle and config.search:
+        print("ERROR - Only one of config values 'shuffle' and 'search' can be True, exiting...")
+        sys.exit(1)
+
+    if config.shuffle:
+        url = 'http://deadendthrills.com/random'
+    elif config.search:
+        try:
+            url = search_game_index(config.game_name)
+        except Exception:
+            print('ERROR - Could not find specified game (' + config.game_name + ') in index, exiting...')
+            sys.exit(1)
+    else:
+        url = 'http://deadendthrills.com'
+
     urls = []
 
     while len(urls) < config.images:
-        urls.extend(get_urls(low_aspect, high_aspect))
+        urls.extend(get_image_urls(url, low_aspect, high_aspect))
     create_directory()
     download(urls)
     sys.exit()
 
 
-def get_urls(low_aspect, high_aspect):
+def get_image_urls(url, low_aspect, high_aspect):
 
     urls = []
 
-    # Create URL soup for parsing with Beautiful Soup
-    url = "http://deadendthrills.com"
-    if config.shuffle:
-        url = url + "/random/"
-
-    ua_spoof_request = urllib.request.Request(url, headers={'User-Agent': r'Mozilla/5.0 \
-    (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'})
-
-    try:
-        html_text = urllib.request.urlopen(ua_spoof_request).read()
-    except urllib.error.HTTPError as exception:
-        sys.exit("ERROR - Could not get Dead End Thrills page for the following reason: " + format(exception))
-
-    soup = BeautifulSoup(html_text, 'html.parser')
+    soup = extract_soup(url)
 
     # Find all 'a' tags with a href to a PNG
     for a in soup.find_all('a'):
@@ -71,6 +73,61 @@ def get_urls(low_aspect, high_aspect):
                     urls.append(a.get('href'))
 
     return urls
+
+
+def extract_soup(url):
+
+    print('Extracting soup for URL: ' + url)
+
+    ua_spoof_request = urllib.request.Request(url, headers={'User-Agent': r'Mozilla/5.0 \
+    (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+    Chrome/61.0.3163.100 Safari/537.36'})
+
+    try:
+        html_text = urllib.request.urlopen(ua_spoof_request).read()
+    except urllib.error.HTTPError as exception:
+        sys.exit("ERROR - Could not get Dead End Thrills page for the following \
+        reason: " + format(exception))
+
+    return BeautifulSoup(html_text, 'html.parser')
+
+
+def search_game_index(game_name):
+    base_url = "http://deadendthrills.com/game-index/?currentpage="
+    pages_left = True
+    page_number = 1
+    game_url = None
+
+    soup = extract_soup(base_url + str(page_number))
+
+    while pages_left:
+        try:
+            game_url = search_game_index_page(game_name, soup)
+        except Exception:
+            print('Game not found on index page ' + str(page_number) + ', checking next page')
+
+        if game_url:
+            return game_url
+
+        page_number += 1
+
+        prev_soup = soup
+        soup = extract_soup(base_url + str(page_number))
+
+        if prev_soup.body.section == soup.body.section:
+            pages_left = False
+
+    raise Exception('Game not found')
+
+
+def search_game_index_page(game_name, soup):
+
+    for div in soup.find_all("div", "gameindexblock"):
+        for child in div.h2:
+            if str(child).lower() == game_name.lower():
+                return div.parent.parent.parent.parent.get('href')
+
+    raise Exception('Game not found')
 
 
 def create_directory():
--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Pixels wide
 width = 1920
 # Pixels high
-height = 1200
+height = 1080
 # Accuracy of matches where 100 is exactly the specified resolution and 0 is any
 fuzziness = 80
 
@@ -12,7 +12,11 @@ fuzziness = 80
 # Number of images to download
 images = 10
 # Whether to shuffle (default is most recent)
-shuffle = True
+shuffle = False
+# Whether to search for a specific game
+search = True
+# Name of the game to search for
+game_name = "Mirror's Edge"
 
 # UPDATE #
 
@@ -22,5 +26,4 @@ number = 1
 
 # FILE SYSTEM #
 
-download_location = ".\\Wallpapers"
-
+download_location = "./Wallpapers"
